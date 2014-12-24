cb9213aed9b2354344bd5459c967765a62ac5251
Romain Monceau <romain@akeneo.com>
2014-12-24 09:28:38 +0100

    Add Akelia bundle

---
+++ FitnAkeliaBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Akelia package.
+ *
+ * (c) Romain Monceau
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fitn\Bundle\AkeliaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle connecting Akeneo PIM and Thelia platform
+ *
+ * @author Romain Monceau <romain@akeneo.com>
+ */
+class FitnAkeliaBundle extends Bundle
+{
+}
